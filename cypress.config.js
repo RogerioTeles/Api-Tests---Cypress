a366d8c77e80886e8a55a7c32994d28cd32a0f61
Rogerio <papulelinho@hotmail.com>
2024-04-01 21:22:34 -0300

    Updating Cypresds version

---
+++ cypress.config.js
@@ -0,0 +1,19 @@
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+  reporter: 'mochawesome',
+  reporterOptions: {
+    charts: true,
+    overwrite: false,
+    html: false,
+    json: true,
+    reportDir: 'cypress/report/mochawesome-report',
+  },
+  e2e: {
+    // We've imported your old cypress plugins here.
+    // You may want to clean this up later by importing these.
+    setupNodeEvents(on, config) {
+      return require('./cypress/plugins/index.js')(on, config)
+    },
+  },
+})
